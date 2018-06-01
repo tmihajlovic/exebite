633cdb55999fe8415333c2c8a906223cb9d7ca51
Goran Mocelj <goran.mocelj@outlook.com>
2018-06-01 11:03:22 +0200

    Updated connection string and catalog name

--- Exebite.DataAccess/Context/FoodOrderingContextFactory.cs
+++ Exebite.DataAccess/Context/FoodOrderingContextFactory.cs
@@ -5,7 +5,7 @@ namespace Exebite.DataAccess.Migrations
 {
     public class FoodOrderingContextFactory : IFoodOrderingContextFactory, IDesignTimeDbContextFactory<FoodOrderingContext>
     {
-        private static string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Exebite.DataAccess.Context.FoodOrderingContext;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        private static string connString = @"Data Source=(local);Initial Catalog=Exebite;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
         private readonly DbContextOptions<FoodOrderingContext> options = new DbContextOptionsBuilder<FoodOrderingContext>().UseSqlServer(connString).UseLazyLoadingProxies().Options;
 
