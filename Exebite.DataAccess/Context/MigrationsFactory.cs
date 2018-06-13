91069e1b441327a16375e2f435572c363aef88f4
Mladen Stanojevic <mstanojevic@execom.eu>
2018-06-13 16:30:00 +0200

    Added context factory used only in migrations

---
+++ Exebite.DataAccess/Context/MigrationsFactory.cs
@@ -0,0 +1,25 @@
+﻿using Exebite.DataAccess.Migrations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exebite.DataAccess.Context
+{
+    /// <summary>
+    /// Used only for migrations
+    /// </summary>
+    public class MladenTestDbContextFactory : IDesignTimeDbContextFactory<FoodOrderingContext>
+    {
+        public FoodOrderingContext CreateDbContext(string[] args)
+        {
+
+            var dbContextOptions = new DbContextOptionsBuilder<FoodOrderingContext>()
+                .UseSqlServer("Server=(LocalDb)\\MSSQLLocalDB;Database=Exebite;Trusted_Connection=True;")
+                .UseLazyLoadingProxies().Options;
+
+            return new FoodOrderingContext(dbContextOptions);
+        }
+    }
+}
