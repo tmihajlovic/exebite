26d4a8ccca6167ae50f5714ba431a17cd0e80cb0
Goran Mocelj <goran.mocelj@outlook.com>
2018-06-25 13:31:34 +0200

    Added Option and Either implementations

---
+++ Option/EnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Option
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<TResult> Flatten<T, TResult>(
+            this IEnumerable<T> sequence, Func<T, Option<TResult>> map) =>
+            sequence.Select(map)
+                .OfType<Some<TResult>>()
+                .Select(x => (TResult)x);
+
+        public static Option<T> FirstOrNone<T>(
+            this IEnumerable<T> sequence, Func<T, bool> predicate) =>
+            sequence.Where(predicate)
+                .Select<T, Option<T>>(x => x)
+                .DefaultIfEmpty(None.Value)
+                .First();
+    }
+}
---
+++ Option/Option.cs
@@ -0,0 +1,34 @@
+﻿namespace Option
+{
+    public abstract class Option<T>
+    {
+        public static implicit operator Option<T>(T value) =>
+            new Some<T>(value);
+
+        public static implicit operator Option<T>(None none) =>
+            new None<T>();
+    }
+
+    public class Some<T> : Option<T>
+    {
+        private T Content { get; }
+
+        public Some(T content)
+        {
+            this.Content = content;
+        }
+
+        public static implicit operator T(Some<T> value) =>
+            value.Content;
+    }
+
+    public class None<T> : Option<T>
+    {
+    }
+
+    public class None
+    {
+        public static None Value { get; } = new None();
+        private None() { }
+    }
+}
