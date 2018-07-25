6fdb9cd806be6f8cb98a713bfb3bffcf3d1dc3f0
Mladen Stanojevic <mstanojevic@execom.eu>
2018-07-25 13:24:13 +0200

    Added HtmlComposer

---
+++ Exebite.Common/HtmlComposer/IHtmlComposer.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Exebite.Common
+{
+    public interface IHtmlComposer
+    {
+        Task<string> ComposeFromPath<T>(string path, T model);
+        Task<string> ComposeFromString<T>(string definition, T model);
+        Task<string> ComposeFromString(string definition);
+    }
+}
