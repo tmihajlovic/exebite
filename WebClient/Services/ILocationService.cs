3cc158557736974d0ee848087ed1761691f2687a
Aleksandar Nikolic <nikolic95@gmail.com>
2018-08-07 16:46:44 +0200

    Add location service interface

---
+++ WebClient/Services/ILocationService.cs
@@ -0,0 +1,20 @@
+﻿using Exebite.Common;
+using Exebite.DtoModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebClient.Services
+{
+    public interface ILocationService
+    {
+        Task<int> CreateAsync(CreateLocationDto locationDto);
+
+        Task<PagingResult<LocationDto>> QueryAsync(LocationQueryDto queryDto);
+
+        Task<bool> DeleteByIdAsync(int id);
+
+        Task<bool> UpdateAsync(int id, UpdateLocationDto locationDto);
+    }
+}
