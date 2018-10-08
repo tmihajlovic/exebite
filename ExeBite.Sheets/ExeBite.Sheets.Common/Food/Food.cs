cbb2faf79d725f5145dfcf6b1cb4367abf494271
Zlatko Radojcic <zradojcic@execom.eu>
2018-10-08 11:48:27 +0200

    Added description of parameters in food class

--- ExeBite.Sheets/ExeBite.Sheets.Common/Food/Food.cs
+++ ExeBite.Sheets/ExeBite.Sheets.Common/Food/Food.cs
@@ -26,16 +26,16 @@
         /// Standard constructor.
         /// Protected so that only child classes can be instantiated.
         /// </summary>
-        /// <param name="name"></param>
-        /// <param name="price"></param>
-        /// <param name="restaurant"></param>
-        /// <param name="subcategory"></param>
-        protected Food(string name, double price, string restaurant, Category scategory)
+        /// <param name="name">Name of the food</param>
+        /// <param name="price">Food price in RSD</param>
+        /// <param name="restaurant">Restaurant name</param>
+        /// <param name="category">Category and subcategory of the food</param>
+        protected Food(string name, double price, string restaurant, Category category)
         {
             Name = name;
             Price = price;
             Restaurant = restaurant;
-            Category = scategory;
+            Category = category;
         }
     }
 }
