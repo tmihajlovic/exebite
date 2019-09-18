884ea840fc8dff8f4e04de1b6a73443a693e4530
Dejan Nadlački <dnadlacki@execom.eu>
2019-09-18 12:07:43 +0200

    Implement extension methods needed for the rest of the connectors. (#29)
    * Add SheetsController.cs that holds the RCP endpoint. Update Startup.cs to use BusinessServices.  Update Index.cshtml for easier access to Sheets API.
    
    * Add GoogleSheetAPI module services provider. Remove unused provider wrapper (also had some bugs). Update Startup.cs.
    
    * Update GoogleSheetServiceFactory service retrieval.
    
    Changing authentication method of web server from OAuth to Service Account credentials.
    Credentials location is currently stored in Properties, but will change in the future.
    
    * Refactor Google Sheet ID access
    
    Change from having an implemented method for every possible restaurant, to easily-changed enumeration.
    Update existing connectors to new sheet ID access.
    Update Sheet IDs of changed Google Sheets.
    
    * Implement extension methods needed for the rest of the connectors.

---
+++ Exebite.GoogleSheetAPI/Extensions/EnumsExtensions.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Exebite.DomainModel;
+using Exebite.GoogleSheetAPI.Enums;
+
+namespace Exebite.GoogleSheetAPI.Extensions
+{
+    /// <summary>
+    /// Extension method for manipulating Enumeration classes.
+    /// </summary>
+    public static class EnumsExtensions
+    {
+        /// <summary>
+        /// Get a String representation of FoodType enum.
+        /// </summary>
+        /// <param name="type">FoodType</param>
+        /// <returns>string value of FoodType</returns>
+        public static string ConvertToString(this FoodType type)
+        {
+            switch (type)
+            {
+                case FoodType.SIDE_DISH:
+                    return "Prilog";
+                case FoodType.SALAD:
+                    return "Salata";
+                case FoodType.DESERT:
+                    return "Desert";
+                case FoodType.SOUP:
+                    return "Supa";
+                case FoodType.CONDIMENTS:
+                    return "Dodatak";
+                case FoodType.MAIN_COURSE:
+                default:
+                    return "Glavno jelo";
+            }
+        }
+
+        /// <summary>
+        /// Get a String representation of ESheetOwner enum. This data should match database information.
+        /// </summary>
+        /// <param name="owner">ESheetOwner</param>
+        /// <returns>string value of ESheetOwner</returns>
+        public static string ConvertToString(this ESheetOwner owner)
+        {
+            switch (owner)
+            {
+                case ESheetOwner.LIPA:
+                    return "Kuhinjica pod Lipom";
+                case ESheetOwner.INDEX_HOUSE:
+                    return "Index House";
+                case ESheetOwner.DE_PAPAJ:
+                    return "De Papaj";
+                case ESheetOwner.MIMAS:
+                    return "Mima's";
+                case ESheetOwner.KASA:
+                    return "Kasa";
+                case ESheetOwner.SERPICA:
+                case ESheetOwner.HEDONE:
+                case ESheetOwner.TEGLAS:
+                    throw new ArgumentException("Obsolete.");
+                default:
+                    throw new ArgumentException("Chosen sheet owner does not exist.");
+            }
+        }
+
+        /// <summary>
+        /// Try to convert string value in sheets to a FoodType enum representation.
+        /// </summary>
+        /// <param name="value">string value of FoodType</param>
+        /// <returns>FoodType enum</returns>
+        public static FoodType ConvertToFoodType(this string value)
+        {
+            switch (value)
+            {
+                case "Prilog":
+                    return FoodType.SIDE_DISH;
+                case "Salata":
+                    return FoodType.SALAD;
+                case "Desert":
+                    return FoodType.DESERT;
+                case "Supa":
+                    return FoodType.SOUP;
+                case "Dodatak":
+                    return FoodType.CONDIMENTS;
+                case "Glavno jelo":
+                default:
+                    return FoodType.MAIN_COURSE;
+            }
+        }
+
+        /// <summary>
+        /// Convert price of the food to FoodType enum. Should only be used for restaurants that have free condiments.
+        /// </summary>
+        /// <param name="price">Price of the food.</param>
+        /// <returns>FoodType</returns>
+        public static FoodType IndexHouseFoodTypeFromPrice(this decimal price)
+        {
+            return price > 0m ? FoodType.MAIN_COURSE : FoodType.CONDIMENTS;
+        }
+    }
+}
---
+++ Exebite.GoogleSheetAPI/Extensions/MappingExtensions.cs
@@ -0,0 +1,52 @@
+﻿﻿using System;
+using System.Collections.Generic;
+
+namespace Exebite.GoogleSheetAPI.Extensions
+{
+    public static class MappingExtensions
+    {
+        /// <summary>
+        /// Parse and map string value of a Customer Name into Location name.
+        /// </summary>
+        /// <param name="name">String value of Customer Name.</param>
+        /// <returns>Location object.</returns>
+        public static string MapLocationName(this string name)
+        {
+            return name.EndsWith("JD")
+                ? "Execom JD"
+                : name.EndsWith("MM")
+                    ? "Execom MM"
+                    : "Execom VS";
+        }
+
+        /// <summary>
+        /// Try to extract and convert value from the sheet onto the object. If it fails, it will return the callback value.
+        /// This method makes sure there's always a value, even if an exception occurs.
+        /// </summary>
+        /// <typeparam name="T">Data Type of the cell.</typeparam>
+        /// <param name="objectList">List of objects.</param>
+        /// <param name="index">Index to access.</param>
+        /// <param name="fallback">Fallback value to return in case of an error.</param>
+        /// <returns>T</returns>
+        public static T ExtractCell<T>(this IList<object> objectList, int index, T fallback)
+        {
+            T retVal;
+
+            try
+            {
+                retVal = (T)Convert.ChangeType(objectList[index], typeof(T));
+
+                if (retVal is string a && string.IsNullOrWhiteSpace(a))
+                {
+                    retVal = fallback;
+                }
+            }
+            catch (Exception)
+            {
+                retVal = fallback;
+            }
+
+            return retVal;
+        }
+    }
+}
