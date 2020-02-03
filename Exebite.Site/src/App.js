809ae915da7c059fb99ebf4a2fd61786d129a76f
Goran Mocelj <goran.mocelj@outlook.com>
2020-02-03 14:58:59 +0100

    Getting food from DB, and showing it as a list on UI

--- Exebite.Site/src/App.js
+++ Exebite.Site/src/App.js
@@ -1,14 +1,55 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './index.css'
+import Table from './Table'
+import Form from './Form'
 
-function App() {
-  return (
-    <div className="App">
-      <p>
-        Hello to Exebite
-      </p>
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: [],
+  }
+
+  removeRow = index => {
+    const { data } = this.state
+
+    this.setState({
+      data: data.filter((row, i) => {
+        return i !== index
+      }),
+    })
+  }
+
+  componentDidMount() {
+    const url =
+      'http://localhost:6879/api/food/Query?Page=1&Size=100';
+
+    fetch(url, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(result => result.json())
+      .then(result => {
+        this.setState({
+          data: result.items
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+
+  render() {
+    const { data } = this.state
+
+    return (
+      <div className="App">
+        <h1>Hello, Exebite!</h1>
+        <div className="container">
+          <h2>Foods:</h2>
+          <Table data={data} removeRow={this.removeRow} />
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
